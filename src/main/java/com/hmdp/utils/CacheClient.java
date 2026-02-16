d90de556f786d2d946708afc28680a0b7afb3a90
QiCheng Wang <2139317487@qq.com>
2026-02-16 23:14:07 +0800

    商户查询缓存

---
+++ src/main/java/com/hmdp/utils/CacheClient.java
@@ -0,0 +1,14 @@
+package com.hmdp.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Kieran_Chase
+ * @project hm-dianping
+ * @date 2026/2/16
+ */
+@Component
+@Slf4j
+public class CacheClient {
+}
